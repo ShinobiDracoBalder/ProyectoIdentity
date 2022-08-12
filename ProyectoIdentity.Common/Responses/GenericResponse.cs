361bd7be9f4624fb85e15813e742b2d54c05470d
Ninja Hybrid Fornite <43005960+ShinobiDracoBalder@users.noreply.github.com>
2022-08-12 13:44:26 -0500

    Changed  First Projects
    V

---
+++ ProyectoIdentity.Common/Responses/GenericResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace ProyectoIdentity.Common.Responses
+{
+    public class GenericResponse<T>
+    {
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+        public T Result { get; set; }
+        public List<T> ListResults { get; set; }
+    }
+}
